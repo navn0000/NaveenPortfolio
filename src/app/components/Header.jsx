6c32f129c6239059414a987f3285c029fcd428c2
navn0000 <naveenkumar.bndar@gmail.com>
2023-08-07 18:41:07 +0530

    test 7

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,9 +2,8 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Styles from '../styleSheets/Header.module.css'
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from 'react-bootstrap/Offcanvas'
 import { AiOutlineMenuUnfold } from 'react-icons/ai';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Header() {

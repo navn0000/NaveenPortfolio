23aa4a274dcc4042de3368945e2437d437e06bb7
navn0000 <naveenkumar.bndar@gmail.com>
2023-09-06 00:36:42 +0530

    error fixing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-"use client"
 import './globals.css';
 import { Inter } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+const metadata = {
   title: 'Naveenkumar Ramkumar',
   description: `Hello there! I'm NaveenKumar, a passionate software developer with a dedication to crafting digital solutions that make a difference. Here, you'll find a glimpse into my journey, expertise, and insights into the ever-evolving world of software development.`,
 }
